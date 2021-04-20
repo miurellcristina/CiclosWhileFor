5b28b1a9e2abcc1e2c7c03d33d1b8eb295639db9
Miurell Cristina <miurellcristina@hotmail.es>
2021-04-19 22:26:32 -0700

    Agregar archivos de proyecto.

---
+++ CiclosWhileFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosWhileFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //For
+            //Parte 1: Valor de la iteracion
+            //Parte 2: Condicion 
+            //Parte 3: Aumento o decremento
+
+            int tabla;
+            int i = 1; 
+            
+            // parte 1
+            Console.WriteLine(" Ingrese un numero de la tabla que quiere calcular");
+            tabla = int.Parse(Console.ReadLine());
+
+            //parte 2
+            while (i<= 10)
+            {
+                Console.WriteLine(" " + tabla * i);
+                //parte 3
+                i++; 
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
